8cae238c87d2b1fcfda6a6273060832272696caf
Braulio Rubio <braulio.rubio01@utrgv.edu>
2024-10-15 12:15:27 -0500

    Modified About Section

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -8,22 +8,13 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2 grid grid-cols-4 gap-4">
-        <li>Node.js</li>
-        <li>Next.js</li>
-        <li>PostgreSQL</li>
-        <li>Unity</li>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Godot</li>
-        <li>C#</li>
-        <li>Python</li>
-        <li>C++</li>
-        <li>OpenCV</li>
-        <li>Git</li>
-        <li>Google Cloud Platform</li>
-        <li>LLM</li>
-        <li>Deep Learning</li>
-        <li>VR/AR</li>
+        <li>Soldering</li>
+        <li>Fortnite</li>
+        <li>Team Fortress 2</li>
+        <li>Screen Plays</li>
+        <li>Cybersecurity</li>
+        <li>Piano</li>
+       
       </ul>
       
     ),
@@ -33,8 +24,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Master&apos;s Computer Science - University of Texas Rio Grande Valley</li>
-        <li>Bachelor&apos;s Computer Science - University of Texas Rio Grande Valley</li>
+        <li>High School - PSJA North Earl College High School</li>
       </ul>
     ),
   },
@@ -43,7 +33,7 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Add Later</li>
+        <li>Google Cybersecurity Certificate</li>
       </ul>
     ),
   },
@@ -66,10 +56,8 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a Game Developer with interest of many topics, from Machine Learning, to Theory.
-            Different skills I have acquired over the years to consider myself a Jack of All Trades, and
-            master to game development since its quite fun.
-
+            I am a Computer Science student at UTRGV (Edingburg) I like games and learning new things.
+            
             My enthusiasm for technology drives me to continue to learn something new, and I have enjoyed
             learning different topics and also teaching others to pursue certain topics if it finds their
             intereset. 
